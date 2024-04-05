30e29ce093c720071ef7158020c69a3added91e6
KevinFritsch <kevinfritsch7@yahoo.de>
2024-04-05 09:11:26 +0200

    Added LOGGER.info on line 69 and 70

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -65,6 +65,9 @@ public class ExampleMod
     {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
+            // Some client setup code
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
         }
     }
 }
