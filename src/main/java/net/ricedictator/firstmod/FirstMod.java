2326d93b8f6eca994a0ece9ab53f1dc262d5d3cd
KevinFritsch <kevinfritsch7@yahoo.de>
2024-04-06 11:12:14 +0200

    Added Sapphire as an item into the game.
    Might not work but i will still commit and comment afterwards if it works

--- src/main/java/net/ricedictator/firstmod/FirstMod.java
+++ src/main/java/net/ricedictator/firstmod/FirstMod.java
@@ -2,14 +2,17 @@ package net.ricedictator.firstmod;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.ricedictator.firstmod.newItem.newModItem;
 import org.slf4j.Logger;
 
 @Mod(FirstMod.MOD_ID)
@@ -20,14 +23,20 @@ public class FirstMod {
     public FirstMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
+        newModItem.register(modEventBus);
         modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(this::addCreative);
 
         MinecraftForge.EVENT_BUS.register(this);
     }
-
+    private void addCreative(BuildCreativeModeTabContentsEvent event){
+        if(event.getTabKey() == CreativeModeTab.INGREDIENTS){
+            event.accept(newModItem.SAPPHIRE);
+        }
+    }
     private void commonSetup(final FMLCommonSetupEvent event){
     }
+
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
